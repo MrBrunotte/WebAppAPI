8fdafa44b981ff1e320515a916a6425c1da55c93
StefanBrunotte <stefan.brunotte@prog-it.se>
2022-03-30 16:47:18 +0200

    Add project files.

---
+++ WebApp/Controllers/WeatherForecastController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        [HttpGet]
+        public string GetName()
+        {
+            return "Stefan Brunotte";
+        }
+        [Route("getAge")]
+        [HttpGet]
+        public int GetAge()
+        {
+            return 50;
+        }
+    }
+}
